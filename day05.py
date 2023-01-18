afded5fff2621c3f415f7ecbf91584ce7aac6b1f
NeulboGom <jinhee930409@inha.edu>
2023-01-18 10:48:14 +0900

    day05 Making Function

--- day05.py
+++ day05.py
@@ -1,72 +1,31 @@
 #Chapter8 Set
 
-test_str=set("letters")
-print(test_str)
+#두 수 사이의 소수를 전부 나열하는 코드
 
-test_dict={"apple":'red','oragne':'oragne'}
-test_set=set(test_dict)
-print(test_set)
+def isprime(n):
+        '''
+        매개변수로 받은 정수가 prime number인지 판정하는 함수입니다.
+        parameter n: integer number
+        return: True or False
+        '''
+        if n <= 1:
+                return False
+        elif n > 1:
+                for k in range(2, n):
+                        if n % k == 0:
+                                return False
+                else:
+                        return True
 
-drinks={"martini":{'vodka','vermouth'},
-        'black russian':{"vodka",'kahlua'},
-        "white russian":{"cream","kahlua","vodka"},
-        "manhattan":{"rye","vermouth","bitters"},
-        "screwdriver":{"orange juice","vodka"}}
-'''
-for name, contents in drinks.items():
-        if 'vodka' in contents and not ("vermouth" in contents or 'cream' in contents):
-                print(name)
+print(isprime(19))
+help(isprime)
 
-for name, contents in drinks.items():
-        if contents & {'vermouth','orange juice'}:
-                print(name)
-'''
+num1=int(input("number1:"))
+num2=int(input("number2:"))
 
-for name, contents in drinks.items():
-        if 'vodka' in contents and not contents & {"vermouth","cream"}:
-                print(name)
+if num1>num2:
+    num1,num2=num2,num1
 
-
-bruss=drinks["black russian"]
-wruss=drinks["white russian"]
-
-#교집합 구하기 & 와 .intersection()
-print(bruss & wruss)
-print(bruss.intersection(wruss))
-print("="*80)
-
-#합집합 구하기
-print(bruss|wruss) #'shift+\'=|
-print(bruss.union(wruss))
-print("="*80)
-
-#차집합
-print(bruss-wruss)
-print(wruss-bruss)
-print(wruss.difference(bruss))
-print("="*80)
-
-#대칭 차집합 구하기: 한 셋에는 포함할 수 있지만 두 셋 모두에는 포함되지 않는 항목 // 교집합의 여집합
-print(bruss^wruss)
-print(bruss.symmetric_difference(wruss))
-print("="*80)
-
-#부분집합 확인   부분집합<=상위집합 형태 // 부분집합.issubset(상위집합) 형태
-#상위 집합은 부분집합의 반대.
-print(wruss<=bruss)
-print(bruss.issubset(wruss))
-print("="*80)
-
-#진부분집합: 한 셋이 다른 셋의 모든 contents를 포함하고 있어야 한다.
-print(bruss<wruss)
-print(wruss<bruss)
-print("="*80)
-
-#Set Comprehension
-a_set={number for number in range(1,6) if number%3 == 1}
-print(a_set)
-print("="*60)
-
-#불변 셋 생성하기:frozenset()
-list_frozen=frozenset([3,2,1])
-print(list_frozen)
+for i in range(num1,num2+1):
+   if isprime(i):
+       print(i,end=' ')
