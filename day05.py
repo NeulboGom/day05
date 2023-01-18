b750ec76d3348e744805e95a0108145d777d9a34
NeulboGom <jinhee930409@inha.edu>
2023-01-18 15:45:02 +0900

    Chapter9 Function: Lambda

--- day05.py
+++ day05.py
@@ -1,50 +1,51 @@
 #Chapter9 Fucntion
-'''
-def substract(n1,n2):
-    print(n1-n2)
 
-def run_func(f,arg1,arg2):
-    f(arg1,arg2)
+#Lamda Function 익명함수
+stairs=["thud","meow","thud","hiss"]
 
-run_func(substract, 99, 98)'''
-
-
-def substract(n1,n2):
-    return n1-n2
-
-def run_func(f,arg1,arg2):
-    f(arg1,arg2)
-
-print(run_func(substract, 99, 98))
+def edit_story(words, func):
+    for word in words:
+        print(func(word))
 
 
-#함수는 객체로 다뤄지고 매개변수로도 쓸 수 있고
-#Return 용도로도 쓸 수 있다.
+def enliven(word):  # 첫 글자를 대문자로 만들고 느낌표 붙이기
+    return word.capitalize() + '!'
 
 
-def caculate():
-    x,y=1,2
-    temp=0
-    def add_sub(n):
-        nonlocal temp
-        #x=11
-        temp = temp+x+n-y
-        return temp
-
-    return add_sub
+edit_story(stairs,enliven)
 
 
-c1=caculate()
-for i in range(10):
-    print(c1(i))
 
 
-def outer(a,b):
+#rnadint로 100까지의 수 중에 제곱을 하는 것 5개의 수를 만들어보자..
 
-    def inner(c,d):
-        return c+d
+"""
+def squares(n):
+    '''
+    정수를 제곱하는 함수
+    :param n: integer
+    :return: integer
+    '''
+    return n*n
 
-    return inner(a,b)
+import random
 
+def process(n_list, function):
+    for n in n_list:
+        print(function(n))
 
-print(outer(4,7))
+numbers=[random.randint(1,100) for number in range(5)]
+print(numbers)
+process(numbers,squares)
+"""
+
+import random
+
+def process(n_list, function):
+    for n in n_list:
+        print(function(n))
+
+numbers=[random.randint(1,100) for number in range(5)]
+print(numbers)
+process(numbers,lambda x: x * x)    #Lambda는 굳이 함수를 안 만들고, 익명의 함수를 만들어서 일시적으로 처리
+                                    #Lambda가 process함수 중 function에 할당되고, Lambda의 x에 numbers의 n이 대입
